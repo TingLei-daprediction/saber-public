ecb05299ddd540a0764396ceaf1bfad9e1c1bbd7
Tinglei-daprediction <leiting2002@gmail.com>
2025-12-04 01:02:42 +0000

    WIP

--- src/saber/mgbf/mgbf_lib/mg_filtering.f90
+++ src/saber/mgbf/mgbf_lib/mg_filtering.f90
@@ -1146,7 +1146,7 @@ include "type_intstat_point2this.inc"
         do k=1,km2
           lev1=lev2+1
           lev2=lev1
-          call this%rbeta(1,hx,1,im,this%paspx4d(lm:lm,1:im,j,1),this%ssx4d(lm:lm,1:im,j,2),HALL(lev1:lev2,:,j))
+          call this%rbeta(1,hx,1,im,this%paspx4d(lm:lm,1:im,j,2),this%ssx4d(lm:lm,1:im,j,2),HALL(lev1:lev2,:,j))
           lev1=lev1+1
           lev2=lev2+1
         enddo
