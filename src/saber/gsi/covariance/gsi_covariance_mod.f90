0f13368b404f9f62775bc3d5afe71c8710a49047
Tinglei-daprediction <leiting2002@gmail.com>
2026-02-12 20:12:31 +0000

    WIP merging

--- src/saber/gsi/covariance/gsi_covariance_mod.f90
+++ src/saber/gsi/covariance/gsi_covariance_mod.f90
@@ -988,6 +988,7 @@ end subroutine multiply
 !
    real(kind=kind_real), allocatable :: t_pt(:,:,:)
    real(kind=kind_real), pointer ::    tv(:,:,:)=>NULL()
+   real(kind=kind_real), pointer ::     t(:,:,:)=>NULL()
    real(kind=kind_real), pointer :: tv_pt(:,:,:)=>NULL()
    real(kind=kind_real), pointer ::     q(:,:,:)=>NULL()
    real(kind=kind_real), pointer ::  q_pt(:,:,:)=>NULL()
@@ -1020,7 +1021,7 @@ end subroutine multiply
       endif
       ! retrieve missing field
       if(which=='tlm') then
-        call gsi_tv_to_t_tl(tv,tv_pt,q,q_pt,t_pt)
+        call gsi_tv_to_t_tl(tv,tv_pt,q,q_pt,t_pt,t)
         ! pass it back to JEDI ...
         allocate(aux1(size(rank2,2)))
         if (vflip) then
@@ -1040,7 +1041,7 @@ end subroutine multiply
         endwhere
       endif
       if(which=='adm') then
-        call gsi_tv_to_t_ad(tv,tv_pt,q,q_pt,t_pt)
+        call gsi_tv_to_t_ad(tv,tv_pt,q,q_pt,t_pt,t)
         where(need=='tv')
            need='filled-'//need
         endwhere
@@ -1101,6 +1102,7 @@ end subroutine multiply
 !
    real(kind=kind_real), allocatable :: t_pt(:,:,:)
    real(kind=kind_real), pointer ::       tv(:,:,:)=>NULL()
+   real(kind=kind_real), pointer ::     t(:,:,:)=>NULL()
    real(kind=kind_real), pointer ::    tv_pt(:,:,:)=>NULL()
    real(kind=kind_real), pointer ::        q(:,:,:)=>NULL()
    real(kind=kind_real), pointer ::     q_pt(:,:,:)=>NULL()
@@ -1147,7 +1149,7 @@ end subroutine multiply
       endif
       ! retrieve missing field
       if(which=='tlm') then
-        call gsi_tv_to_t_tl(tv,tv_pt,q,q_pt,t_pt)
+        call gsi_tv_to_t_tl(tv,tv_pt,q,q_pt,t_pt,t)
         where(need=='tv')
            need='filled-'//need
         endwhere
@@ -1167,7 +1169,7 @@ end subroutine multiply
         deallocate(aux1)
       endif
       if(which=='adm') then
-        call gsi_tv_to_t_ad(tv,tv_pt,q,q_pt,t_pt)
+        call gsi_tv_to_t_ad(tv,tv_pt,q,q_pt,t_pt,t)
         where(need=='tv')
            need='filled-'//need
         endwhere
