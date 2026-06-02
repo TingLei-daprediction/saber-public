95b58c5e0b689df7d9c1fda5eca4c191761e30b1
TingLei-daprediction <leiting2002@gmail.com>
2026-06-02 23:23:01 +0000

    WIP

--- src/saber/mgbf/mgbf_lib/mgbf_kind.f90
+++ src/saber/mgbf/mgbf_lib/mgbf_kind.f90
@@ -36,7 +36,7 @@ module mgbf_kinds
 !   machine:  ibm RS/6000 SP
 !
 !$$$ end documentation block
-  use iso_fortran_env, only: int64
+  use, intrinsic ::iso_fortran_env, only: int64
   implicit none
   private
 
@@ -113,11 +113,7 @@ module mgbf_kinds
        real_byte_sizes( default_real )
   integer,parameter, public :: spc=kind((1.0,1.0))
   integer,parameter, public :: dpc=kind((1.0d0,1.0d0))
-  private:: one_dpi; integer(8),parameter:: one_dpi=1
-
-
   integer(int64), parameter :: one_dpi = 1   ! ← portable, always 64-bit
-
   integer,parameter , public :: dpi=kind(one_dpi)
 
 end module mgbf_kinds
