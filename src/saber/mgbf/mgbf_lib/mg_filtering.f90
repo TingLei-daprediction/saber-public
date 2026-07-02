ca9830006b2ee44303d501a3bf027f14e25d0ca3
TingLei-daprediction <leiting2002@gmail.com>
2026-07-02 18:19:06 -0400

    Add OpenMP to vertical filters sup_vrbeta1_bkg and sup_vrbeta1T_bkg

--- src/saber/mgbf/mgbf_lib/mg_filtering.f90
+++ src/saber/mgbf/mgbf_lib/mg_filtering.f90
@@ -1961,6 +1961,7 @@ real(r_kind),dimension(1:km3,1-hz:lm+hz):: W
 integer(i_kind):: i,j,L,k,k_ind,kloc
 !----------------------------------------------------------------------
 
+!$omp parallel do collapse(2) private(i,j,L,k,k_ind,kloc,W) schedule(static)
     do j=1,jm
     do i=1,im
       do k=1,km3
@@ -1986,6 +1987,7 @@ integer(i_kind):: i,j,L,k,k_ind,kloc
       enddo
    enddo
    enddo
+!$omp end parallel do
 
 !----------------------------------------------------------------------
 endsubroutine sup_vrbeta1_bkg
@@ -2009,6 +2011,7 @@ real(r_kind),dimension(1:km3,1-hz:lm+hz):: W
 integer(i_kind):: i,j,L,k,k_ind,kloc
 !----------------------------------------------------------------------
 
+!$omp parallel do collapse(2) private(i,j,L,k,k_ind,kloc,W) schedule(static)
         do j=1,jm
         do i=1,im
 
@@ -2043,6 +2046,7 @@ integer(i_kind):: i,j,L,k,k_ind,kloc
 
         end do
         end do
+!$omp end parallel do
 
 !----------------------------------------------------------------------
 endsubroutine sup_vrbeta1T_bkg
