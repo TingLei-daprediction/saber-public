7067fea959320ad93abb571d0227bd393b824468
Benjamin Menetrier <benjamin.menetrier@met.no>
2026-07-16 14:36:29 +0200

    Cleaning

--- src/saber/mgbf/covariance/MGBF_Covariance.cc
+++ src/saber/mgbf/covariance/MGBF_Covariance.cc
@@ -7,12 +7,113 @@
 
 #include "saber/mgbf/covariance/MGBF_Covariance.h"
 
+#include <iostream>
+
+#include "atlas/field.h"
+
+#include "saber/mgbf/covariance/MGBF_Covariance.interface.h"
+#include "saber/oops/Utilities.h"
+
 namespace saber {
 namespace mgbf {
 
 // -------------------------------------------------------------------------------------------------
 
 static SaberCentralBlockMaker<MGBF_Covariance> makerCovariance_("MGBF_covariance");
+
+// -------------------------------------------------------------------------------------------------
+
+MGBF_Covariance::MGBF_Covariance(const oops::GeometryData & geometryData,
+                                 const oops::Variables & centralVars,
+                                 const eckit::Configuration & covarConf,
+                                 const Parameters_ & params,
+                                 const oops::FieldSet3D & xb,
+                                 const oops::FieldSet3D & fg)
+  : SaberCentralBlockBase(params, xb.validTime(), geometryData, centralVars),
+     params_(params), variables_(params.activeVars.value().get_value_or(centralVars).variables()),
+     mgbfGridFuncSpace_(geometryData.functionSpace()), comm_(&geometryData.comm())
+{
+  oops::Log::trace() << classname() << "MGBF::Covariance starting" << std::endl;
+
+  util::Timer timer(classname(), "Covariance");
+  eckit::LocalConfiguration mgbf_config = params.toConfiguration();
+  if (params.doCalibration()) {
+    throw eckit::UserError("doCalibration=true is not implemented ", Here());
+  }
+
+  // Create covariance module
+  mgbf_covariance_create_f90(keySelf_, *comm_, mgbf_config,
+                            mgbfGridFuncSpace_.get(), xb.get(), fg.get());
+
+  oops::Log::trace() << classname() << "::Covariance done" << std::endl;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+MGBF_Covariance::~MGBF_Covariance() {
+  oops::Log::trace() << classname() << "::~Covariance starting" << std::endl;
+  util::Timer timer(classname(), "~Covariance");
+  mgbf_covariance_delete_f90(keySelf_);
+  oops::Log::trace() << classname() << "::~Covariance done" << std::endl;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void MGBF_Covariance::randomize(oops::FieldSet3D & fset) const {
+  oops::Log::trace() << classname() << "::randomize starting" << std::endl;
+  util::Timer timer(classname(), "randomize");
+
+  for (auto sabField : fset) {
+    // Get the name
+    const auto fieldName = name(sabField.name());
+
+    // Ensure that the field name is in the input/output list
+    const std::string fieldNameStr = fieldName.getString("name");
+    if (std::find(variables_.begin(), variables_.end(), fieldNameStr) == variables_.end()) {
+      ABORT("Field " + fieldNameStr + " not found in the " + classname() + " variables.");
+    }
+  }
+
+  mgbf_covariance_randomize_f90(keySelf_, fset.get());
+  oops::Log::trace() << classname() << "::randomize done" << std::endl;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void MGBF_Covariance::multiply(oops::FieldSet3D & fset) const {
+  oops::Log::trace() << classname() << "::multiply starting" << std::endl;
+  util::Timer timer(classname(), "multiply");
+
+  // Get member index
+  int index_member;
+  if (fset.fieldSet().metadata().has("ensemble member index")) {
+    index_member = fset.fieldSet().metadata().get<int>("ensemble member index");
+  } else {
+    index_member = 9999;
+  }
+
+  // Multiply
+  mgbf_covariance_multiply_f90(keySelf_, fset.get(), index_member);
+
+  // Mark all fields as having dirty halos after modification
+  for (const auto & fieldname : fset.field_names()) {
+    atlas::Field field = fset[fieldname];
+    field.set_dirty();  // Mark field as having dirty halos that need to be synchronized
+  }
+
+  // Perform the actual halo exchange
+  fset.fieldSet().haloExchange();
+
+  oops::Log::trace() << classname() << "::multiply done" << std::endl;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void MGBF_Covariance::print(std::ostream & os) const {
+  os << classname();
+}
+
+// -------------------------------------------------------------------------------------------------
 
 }  // namespace mgbf
 }  // namespace saber
--- src/saber/mgbf/covariance/MGBF_Covariance.interface.F90
+++ src/saber/mgbf/covariance/MGBF_Covariance.interface.F90
@@ -31,11 +31,11 @@ public mgbf_covariance_registry
 #define LISTED_TYPE mgbf_covariance
 
 !> Linked list interface - defines registry_t type
-#include "mgbf_tools_linkedlist_interface.fypp"
+#include "oops/util/linkedList_i.f"
 
 !> Global registry
 
-type(registry_type) :: mgbf_covariance_registry
+type(registry_t) :: mgbf_covariance_registry
 
 ! --------------------------------------------------------------------------------------------------
 
@@ -44,7 +44,7 @@ contains
 ! --------------------------------------------------------------------------------------------------
 
 !> Linked list implementation
-#include "mgbf_tools_linkedlist_implementation.fypp"
+#include "oops/util/linkedList_c.f"
 
 ! --------------------------------------------------------------------------------------------------
 
@@ -69,8 +69,7 @@ type(atlas_fieldset)          :: f_fg
 
 ! LinkedList
 ! ----------
-f_comm=fckit_mpi_comm(c_comm)
-call mgbf_covariance_registry%init(f_comm)
+call mgbf_covariance_registry%init()
 call mgbf_covariance_registry%add(c_self)
 call mgbf_covariance_registry%get(c_self, f_self)
 
